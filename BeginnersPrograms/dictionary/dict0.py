1dd98df4edb6b70ba77e03a15058bd1bff580941
Jugob <30793040+Jugob@users.noreply.github.com>
2018-04-27 18:22:38 +0530

    Add files via upload

---
+++ BeginnersPrograms/dictionary/dict0.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+d1={"a":"apple", "b":"banana", "c":"cherry"}
+print(d1)
+print(d1["a"])
+print(d1["b"])
+print(d1["c"])
+print(d1[1])
---
+++ BeginnersPrograms/dictionary/dict2.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+d1={"a":"apple", "b":"banana", "c":"cherry"}
+print(d1)
+d1["a"]="avocado"
+print(d1)
